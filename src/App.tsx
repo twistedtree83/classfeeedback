035c3f381d901dcc1d1fb34718ff80dfdab717d1
twistedtree83 <twistedtree83@users.noreply.github.com>
2025-05-30 18:02:25 +1000

    Start development server

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<TeacherDashboard />} />
         <Route path="/join" element={<JoinPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<Navigate to="/\" replace />} />
       </Routes>
     </BrowserRouter>
   );
